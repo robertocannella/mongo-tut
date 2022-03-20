52f67510212537cdc7903dc4807e1477e9a51db6
Roberto Cannella <robertocannella@gmail.com>
2022-03-20 21:27:28 +0000

    added db functionality

---
+++ index.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+const DATABASEUSERNAME = config.get('db.dbUser');
+const DATABASEPASSWORD = config.get('db.dbPass');
+const DATABASEHOST = config.get('db.dbHost');
+const DATABASEPORT = config.get('db.dbPort');
+const DATABASENAME = config.get('db.dbName');
+
+// connect to mongodb
+const Connect = async () => {
+
+    let url = `mongodb://${DATABASEHOST}:${DATABASEPORT}/${DATABASENAME}`;
+
+    try {
+
+        let client = await mongoose.connect(url, {
+            authSource: "admin",
+            user: DATABASEUSERNAME,
+            pass: DATABASEPASSWORD
+        });
+
+        console.log("Database is connected!");
+    } catch (error) {
+        console.log(error.stack);
+        process.exit(1);
+    }
+
+}
+Connect();
+
+// create model for our objects to store in mongo db
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date().now },
+    isPublished: Boolean
+});
+
+// compile object into mongoos
+const Course = mongoose.model('Course', courseSchema)
+async function createCourse() {
+    // create object
+    const course = new Course({
+        name: 'React Course',
+        author: 'Mosh',
+        tags: ['react', 'front end'],
+        isPublished: true
+    })
+
+    const result = await course.save().catch((e) => { console.log(e) })
+    console.log(result)
+}
+
+createCourse();
